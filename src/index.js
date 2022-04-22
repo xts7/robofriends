9c4fca69b55041d1b06e6767f6b95f12e1c93279
Jan Tutakiewicz <jan.tut.cz@gmail.com>
2022-04-22 17:25:53 +0200

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger} from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
-ReactDOM.render(<App />,  document.getElementById('root'));
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>,  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
